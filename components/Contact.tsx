180f6d6f3fd14c0fa9da69b8766237424b544088
Paul Nogaro <paulnogaro@MacBook-Air-de-Paul.local>
2026-01-28 16:05:56 +0100

    Mise à jour de l'adresse email vers contact@sohoconciergerie.fr

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -66,7 +66,7 @@ export default function Contact() {
                 >
                   <path d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
                 </svg>
-                <span className="text-base sm:text-lg text-gray-400">contact@lespipelettes.fr</span>
+                <span className="text-base sm:text-lg text-gray-400">contact@sohoconciergerie.fr</span>
               </div>
             </div>
           </div>
